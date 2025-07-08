d0a4cebc926cedd5dbc16df9f54658a5c1127464
Andrea Telatin <15690844+telatin@users.noreply.github.com>
2025-07-08 14:38:47 +0100

    countfa: add threads option and stdin

--- src/countfa.c
+++ src/countfa.c
@@ -6,8 +6,11 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#include <getopt.h>
 #define CHUNK_SIZE 1048576 // 1MB chunks
-#define NUM_THREADS 4
+#define DEFAULT_THREADS 4
+
+static int num_threads = DEFAULT_THREADS;
 typedef struct {
     gzFile file;
     size_t newline_count;
@@ -52,39 +55,94 @@ void *count_newlines(void *arg) {
     return NULL;
 }
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <fasta.gz file>\n", argv[0]);
+    int opt;
+    static struct option long_options[] = {
+        {"threads", required_argument, 0, 't'},
+        {0, 0, 0, 0}
+    };
+
+    while ((opt = getopt_long(argc, argv, "t:", long_options, NULL)) != -1) {
+        switch (opt) {
+            case 't':
+                num_threads = atoi(optarg);
+                if (num_threads <= 0) {
+                    fprintf(stderr, "Invalid thread count: %s\n", optarg);
+                    return 1;
+                }
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [--threads N] <fasta.gz file>\n", argv[0]);
+                return 1;
+        }
+    }
+
+    if (optind >= argc) {
+        fprintf(stderr, "Usage: %s [--threads N] <fasta.gz file>\n", argv[0]);
         return 1;
     }
-    gzFile file = gzopen(argv[1], "rb");
+
+    const char *filename = argv[optind];
+    gzFile file;
+    if (strcmp(filename, "-") == 0) {
+        file = gzdopen(fileno(stdin), "rb");
+    } else {
+        file = gzopen(filename, "rb");
+    }
     if (!file) {
-        fprintf(stderr, "Error: Cannot open file %s: %s\n", argv[1], strerror(errno));
+        fprintf(stderr, "Error: Cannot open file %s: %s\n", filename, strerror(errno));
         return 1;
     }
-    pthread_t threads[NUM_THREADS];
-    ThreadData thread_data[NUM_THREADS];
-    for (int i = 0; i < NUM_THREADS; i++) {
+
+    int first = gzgetc(file);
+    if (first == -1) {
+        fprintf(stderr, "Error reading from %s\n", filename);
+        gzclose(file);
+        return 1;
+    }
+    if (first != '>') {
+        fprintf(stderr, "Invalid FASTA format: first character is not '>'\n");
+        gzclose(file);
+        return 1;
+    }
+    gzungetc(first, file);
+
+    pthread_t *threads = malloc(sizeof(pthread_t) * num_threads);
+    ThreadData *thread_data = malloc(sizeof(ThreadData) * num_threads);
+    if (!threads || !thread_data) {
+        fprintf(stderr, "Memory allocation failure\n");
+        gzclose(file);
+        free(threads);
+        free(thread_data);
+        return 1;
+    }
+    for (int i = 0; i < num_threads; i++) {
         thread_data[i].file = file;
         thread_data[i].newline_count = 0;
         thread_data[i].thread_id = i;
         if (pthread_create(&threads[i], NULL, count_newlines, &thread_data[i]) != 0) {
             fprintf(stderr, "Error creating thread %d: %s\n", i, strerror(errno));
             gzclose(file);
+            free(threads);
+            free(thread_data);
             return 1;
         }
     }
-    for (int i = 0; i < NUM_THREADS; i++) {
+    for (int i = 0; i < num_threads; i++) {
         pthread_join(threads[i], NULL);
     }
     gzclose(file);
     if (global_error) {
         fprintf(stderr, "An error occurred during processing. Results may be incomplete.\n");
+        free(threads);
+        free(thread_data);
         return 1;
     }
     size_t total_newlines = 0;
-    for (int i = 0; i < NUM_THREADS; i++) {
+    for (int i = 0; i < num_threads; i++) {
         total_newlines += thread_data[i].newline_count;
     }
     printf("Total sequences: %zu\n", total_newlines);
+    free(threads);
+    free(thread_data);
     return 0;
 }
